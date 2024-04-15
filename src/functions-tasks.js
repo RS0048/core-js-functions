122a893df0fb1ae4fff48a5072e1101691d2ed5e
RS0048 <sokolovrmn@gmail.com>
2024-04-15 18:59:54 +0300

    feat: part tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -176,8 +176,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...arg2) => fn(...args1, ...arg2);
 }
 
 /**
@@ -197,8 +197,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let total = startFrom;
+  return () => {
+    const newTotal = total;
+    total += 1;
+    return newTotal;
+  };
 }
 
 module.exports = {
